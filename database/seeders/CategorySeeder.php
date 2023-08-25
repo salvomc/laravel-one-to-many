1e939c2f95d57fd9106c1eaf76a04940d585752f
salvomc <smcrwrk@gmail.com>
2023-08-25 10:38:14 +0200

    [FEAT] CategorySeeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Category;
 
 class CategorySeeder extends Seeder
 {
@@ -14,6 +16,20 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = [
+            'Frontend', 
+            'Backend', 
+            'Fullstack', 
+            'Design', 
+            'DevOps'
+        ];
+
+        foreach ($categories as $cat) 
+        {
+            $category = new Category();
+            $category->name = $cat;
+            $category->slug = Str::slug($cat, '-');
+            $category->save();
+        }
     }
 }
